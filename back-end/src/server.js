e48401588e3794d2aeb48b42a6ca5d98766ee8a3
DuyTinNguyen182 <duytinnguyen.2004@gmail.com>
2025-10-31 21:02:31 +0700

    Create API for Category and Product

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser');
 
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
+const categoryRoutes = require("./routes/categoryRoutes"); 
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 
@@ -21,6 +23,8 @@ connect();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
